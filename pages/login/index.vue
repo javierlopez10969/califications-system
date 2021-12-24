9ce40954b2093d4e9747ddaef302975181a77a42
Victor <victor.huanqui@usach.cl>
2021-12-24 00:43:42 -0500

    Actualización del Login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container>
-    <v-spacer></v-spacer>
-    <v-row justify="center" align="center">
-      <HomeLoginForm />
-    </v-row>
-  </v-container>
+    <v-container>
+        <v-spacer></v-spacer>
+        <v-row justify="center" align="center">
+            <HomeLoginForm />
+        </v-row>
+    </v-container>
 </template>
